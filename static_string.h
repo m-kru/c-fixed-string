49033777915b83476ed78f77f53aa31fb83c804f
Michał Kruszewski <mkru@protonmail.com>
2024-01-27 17:54:33 +0100

    Minor changes in the header

--- static_string.h
+++ static_string.h
@@ -4,10 +4,18 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-#define SSTR_TYPE8  0x00
-#define SSTR_TYPE16 0x01
-#define SSTR_TYPE32 0x02
-#define SSTR_TYPE64 0x03
+#define SSTR8  0x00
+#define SSTR16 0x01
+#define SSTR32 0x02
+#define SSTR64 0x03
+
+/*
+ * sstr_reset resets static string to be empty.
+ */
+#define sstr_reset(s) do { \
+	s->_buf[0] = 0;        \
+	s->_str._len = 0;      \
+} while(0)
 
 #define sstr_print(str) do {    \
 	if (str._str._len > 0) {    \
@@ -33,11 +41,13 @@ typedef struct {
 	char _buf[8];
 } sstr8_t;
 
-#define sstr_reset(s) do { \
-	s->_buf[0] = 0;        \
-	s->_str._len = 0;      \
-} while(0)
+#define sstr8_init(name, str) sstr8_t name = {{._len = sizeof(str) - 1, ._type = SSTR8}, ._buf = str}
 
+/*
+ * sstr_len returns string current length. The null byte ('\0') is not included
+ * in the string length. Getting string length has O(1) complexity.
+ * The macro works for string values as well as for string pointers.
+ */
 #define sstr_len(s) _Generic((s), \
 	sstr8_t  : _sstr_len,         \
 	sstr8_t* : _sstr_ptr_len      \
@@ -52,8 +62,6 @@ static inline __attribute__((always_inline))
 uint8_t _sstr_ptr_len(sstr8_t *s) {
 	return s->_str._len;
 }
-
-#define sstr8_init(name, str) sstr8_t name = {{._len = sizeof(str) - 1, ._type = SSTR_TYPE8}, ._buf = str}
 
 #define sstr_write_char(s, x) _Generic((s), \
 	sstr8_t*: sstr8_write_char              \
